b08b6dc6bd0c2692ea7b54e9a3e41d3e64d96b66
DriftCloud <zhangxlei2002@gmail.com>
2024-11-23 11:23:32 -0800

    [Env] Add C++ support
    Signed-off-by: DriftCloud <zhangxlei2002@gmail.com>

---
+++ src/c++/lc/lc704.h
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+class LeetCode704 {
+public:
+    int search(vector<int>& nums, int target) {
+
+        return 0;
+        
+    }
+};
---
+++ src/c++/main.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "lc/lc704.h"
+
+int main(void) {
+  std::cout << "This is main." << std::endl;
+  return 0;
+}
